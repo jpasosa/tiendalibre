e8e66a0b6d8b03bccb0e2f58d1368c768e09175b
juampa <jpasosa@gmail.com>
2020-12-23 16:42:39 -0300

    ....

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 define('VERSION', '3.0.2.0');
 
 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 // Configuration
